3adcdc28d8c66b5dc715fa4ca5f233caed81be1a
Youssef Emad <youssefemad@youssefs-mac-4.local>
2019-07-09 09:32:57 -0700

    Initial Commit

---
+++ Todoli/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoli
+//
+//  Created by Youssef Emad on 7/9/19.
+//  Copyright © 2019 Youssef Emad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
